619b1e872b2592e2306cdd49fd295c16537cbee7
Yijun Yu <yijun.yu@gmail.com>
2017-02-24 20:31:57 +0000

    patching yaxx.c from bison 2.3 to 2.4

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
-#BISON = ${HOME}/bison-1.875
-BISON = ../bison-2.1
+X=2.4
+Y=0.13
+BISON=bison-${X}
 
 SUBDIRS=$(patsubst %Makefile,%,$(wildcard */Makefile))
 
 all clean realclean: $(SUBDIRS)
-	for d in $(SUBDIRS); do (cd ./$$d ; $(MAKE) $@ ); done
+	for d in $(SUBDIRS); do (cd ./$$d ; $(MAKE) X=${X} Y=${Y} $@ ); done
 
-setup:
-	cp bison/data/yaxx-0.12-bison-2.1.c /usr/local/share/bison/data
-	cd $(BISON); ./configure --program-suffix=2.1
+setup: ${BISON}
+	if [ ' -d ${BISON} ' ]; then wget https://ftp.gnu.org/gnu/bison/${BISON}.tar.bz2; tar xvfj ${BISON}.tar.bz2; fi
+	cp bison/data/yaxx-${Y}-bison-${X}.c /usr/local/share/bison/data
+	cd $(BISON) && ./configure --program-suffix=${X} && make && make install
--- sql/sql2.h
+++ sql/sql2.h
@@ -1,24 +1,23 @@
-/* A Bison parser, made by GNU Bison 2.3.  */
+
+/* A Bison parser, made by GNU Bison 2.4.  */
 
 /* Skeleton interface for Bison's Yacc-like parsers in C
-
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
    Free Software Foundation, Inc.
-
-   This program is free software; you can redistribute it and/or modify
+   
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
+   
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -29,9 +28,10 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-
+   
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
+
 
 /* Tokens.  */
 #ifndef YYTOKENTYPE
@@ -129,115 +129,31 @@
      WORK = 345
    };
 #endif
-/* Tokens.  */
-#define NAME 258
-#define STRING 259
-#define INTNUM 260
-#define APPROXNUM 261
-#define OR 262
-#define AND 263
-#define NOT 264
-#define COMPARISON 265
-#define UMINUS 266
-#define ALL 267
-#define AMMSC 268
-#define ANY 269
-#define AS 270
-#define ASC 271
-#define AUTHORIZATION 272
-#define BETWEEN 273
-#define BY 274
-#define CHARACTER 275
-#define CHECK 276
-#define CLOSE 277
-#define COMMIT 278
-#define CONTINUE 279
-#define CREATE 280
-#define CURRENT 281
-#define CURSOR 282
-#define DECIMAL 283
-#define DECLARE 284
-#define DEFAULT 285
-#define DELETE 286
-#define DESC 287
-#define DISTINCT 288
-#define DOUBLE 289
-#define ESCAPE 290
-#define EXISTS 291
-#define FETCH 292
-#define FLOAT 293
-#define FOR 294
-#define FOREIGN 295
-#define FOUND 296
-#define FROM 297
-#define GOTO 298
-#define GRANT 299
-#define GROUP 300
-#define HAVING 301
-#define IN 302
-#define INDICATOR 303
-#define INSERT 304
-#define INTEGER 305
-#define INTO 306
-#define IS 307
-#define KEY 308
-#define LANGUAGE 309
-#define LIKE 310
-#define NULLX 311
-#define NUMERIC 312
-#define OF 313
-#define ON 314
-#define OPEN 315
-#define OPTION 316
-#define ORDER 317
-#define PARAMETER 318
-#define PRECISION 319
-#define PRIMARY 320
-#define PRIVILEGES 321
-#define PROCEDURE 322
-#define PUBLIC 323
-#define REAL 324
-#define REFERENCES 325
-#define ROLLBACK 326
-#define SCHEMA 327
-#define SELECT 328
-#define SET 329
-#define SMALLINT 330
-#define SOME 331
-#define SQLCODE 332
-#define SQLERROR 333
-#define TABLE 334
-#define TO 335
-#define UNION 336
-#define UNIQUE 337
-#define UPDATE 338
-#define USER 339
-#define VALUES 340
-#define VIEW 341
-#define WHENEVER 342
-#define WHERE 343
-#define WITH 344
-#define WORK 345
-
 
 
 
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef union YYSTYPE
-#line 3 "sql2.y"
 {
+
+/* Line 1979 of yaxx-0.13-bison-2.4.c  */
+#line 3 "sql2.y"
+
 	int intval;
 	double floatval;
 	char *strval;
 	int subtok;
-}
-/* Line 1799 of yacc.c.  */
-#line 236 "sql2.tab.h"
-	YYSTYPE;
+
+
+
+/* Line 1979 of yaxx-0.13-bison-2.4.c  */
+#line 151 "sql2.tab.h"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
-# define YYSTYPE_IS_TRIVIAL 1
 #endif
 
 extern YYSTYPE yylval;
+
 
